7fc6f586f0ca65d8ac606a0ccc73fb337b19bfc3
PedroAMC <77537974+PedroAMC@users.noreply.github.com>
2022-01-05 10:31:20 +0000

    Criei um módulo para ler o ficheiro CSV e separar as linhas todas por arrays.

--- src/Projeto.java
+++ src/Projeto.java
@@ -1,6 +1,30 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Projeto {
-    public static void main(String[] args) {
-        System.out.println("ola");
+
+    public static void main(String[] args) throws Exception {
+
+        lerFicheiroCSV();
+
+    }
+
+    public static void lerFicheiroCSV() throws FileNotFoundException {
+
+        String line = "";
+        String splitBy = ",";
+
+        try {
+
+            BufferedReader br = new BufferedReader(new FileReader("exemploRegistoNumerosCovid19.csv"));
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(splitBy);
+                System.out.println("data: " + data[0] + " infetados: " + data[2]);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 }
