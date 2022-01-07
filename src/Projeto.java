386f508d92fff2e88c0f2b0955c3ec2ccfe0e7ab
1211506 <1211506@isep.ipp.pt>
2022-01-07 12:19:01 +0000

    erro na leitura do ficheiro

--- src/Projeto.java
+++ src/Projeto.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 public class Projeto {
 
     public static void main(String[] args) throws FileNotFoundException {
+
+        //LerFicheiroCSV();
+        int numeroDeLinhas = ContarLinhasDoFicheiro() - 1;
+
+        String[] listaDeDatas = new String[numeroDeLinhas];
+        int[] listaDeCasosNaoInfetados = new int[numeroDeLinhas];
+       // listaDeCasosNaoInfetados = PreencherValoresDosDadosPrimeiraFase(1,listaDeCasosNaoInfetados);
+        int[] listaDeInfetados = new int[numeroDeLinhas];
+        //listaDeInfetados = PreencherValoresDosDadosPrimeiraFase(2,listaDeInfetados);
+        int[] listaDeHospitalizados = new int[numeroDeLinhas];
+        //listaDeHospitalizados = PreencherValoresDosDadosPrimeiraFase(3,listaDeHospitalizados);
+        int[] listaDeInternadosUCI = new int[numeroDeLinhas];
+        //listaDeInternadosUCI = PreencherValoresDosDadosPrimeiraFase(4,listaDeInternadosUCI);
+        int[] listaDeMortes = new int[numeroDeLinhas];
+        //listaDeMortes = PreencherValoresDosDadosPrimeiraFase(5,listaDeMortes);
+        System.out.println(numeroDeLinhas);
+
+        //por num modulo???
         if (args.length == 6 || args.length == 8) { // verificar se é um numero válido de argumentos
             for (int i = 0; i < args.length; i = i + 2) {
                 switch (args[i]){
@@ -19,12 +37,14 @@ public class Projeto {
                 }
             }
 
+            //LerFicheiroCSV();
+            ContarLinhasDoFicheiro();
 
-            if (verificarExtensãoFicheiro(args[args.length - 2]).equals("csv")){
+            if (VerificarExtensãoFicheiro(args[args.length - 2]).equals("csv")){
                 String ficheiroEntrada = args[args.length - 2];
             } else System.out.println("Ficheiro de entrada inválido");
 
-            if (verificarExtensãoFicheiro(args[args.length - 1]).equals("txt")){
+            if (VerificarExtensãoFicheiro(args[args.length - 1]).equals("txt")){
                 String ficheiroSaida = args[args.length - 1];
 
             } else System.out.println("Ficheiro de saida inválido");
@@ -33,7 +53,45 @@ public class Projeto {
 
 
     }
-    public static String verificarExtensãoFicheiro (String nomeFicheiro){
+ /*   public static int[] PreencherValoresDosDadosPrimeiraFase(int tipoDeDado, int[] listaDeDados) throws FileNotFoundException {
+
+        String splitBy = ",";
+        Scanner fin = new Scanner(new File("exemploRegistoNumerosCovid19.csv"));
+        fin.next();
+        while (fin.next() != null) {
+            String line = fin.next();
+            String[] dados = line.split(splitBy);
+            if (tipoDeDado != 0) {
+                PreencherValorDosDadosSegundaFase(tipoDeDado, dados, listaDeDados);
+            }
+        }
+        return listaDeDados;
+    }*/
+    public static void PreencherValorDosDadosSegundaFase(int tiposDeDados, String[] dados, int[] listaDeDados){
+        for (int i = 0; i < listaDeDados.length; i++) {
+            listaDeDados[i] = Integer.parseInt(dados[tiposDeDados]);
+            System.out.println(listaDeDados[i]);
+        }
+    }
+
+    public static String[] PreencherListaDeDatas(int tipoDeDados,String[] dados, String[] listaDeDatas) {
+
+        return dados;
+    }
+
+    public static int ContarLinhasDoFicheiro() throws FileNotFoundException {
+
+        Scanner fin = new Scanner(new File("exemploRegistoNumerosCovid19.csv"));
+        int counter = 0;
+
+        while (fin.nextLine() != null){
+            counter++;
+        }
+        fin.close();
+        return counter;
+    }
+
+    public static String VerificarExtensãoFicheiro(String nomeFicheiro){
         String ext = "";
 
         int i = nomeFicheiro.lastIndexOf('.');
@@ -44,7 +102,7 @@ public class Projeto {
         return ext;
     }
 
-    public static void lerFicheiroCSV() throws FileNotFoundException {
+   /* public static void LerFicheiroCSV() throws FileNotFoundException {
 
         String line = "";
         String splitBy = ",";
@@ -53,8 +111,9 @@ public class Projeto {
 
             BufferedReader br = new BufferedReader(new FileReader("exemploRegistoNumerosCovid19.csv"));
             while ((line = br.readLine()) != null) {
-                String[] data = line.split(splitBy);
-                System.out.println("data: " + data[0] + " infetados: " + data[2]);
+                String[] dados = line.split(splitBy);
+
+                System.out.println("data: " + dados[0] + " infetados: " + dados[2]);
             }
 
         } catch (IOException e) {
@@ -62,4 +121,4 @@ public class Projeto {
         }
 //teste
     }
-}
+*/}
