8dfa6257ada601919279b7a84fda303b7f0fb37d
1211506 <1211506@isep.ipp.pt>
2022-01-07 09:28:01 +0000

    teste

--- src/Projeto.java
+++ src/Projeto.java
@@ -3,10 +3,45 @@ import java.util.Scanner;
 
 public class Projeto {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws FileNotFoundException {
+        if (args.length == 6 || args.length == 8) { // verificar se é um numero válido de argumentos
+            for (int i = 0; i < args.length; i = i + 2) {
+                switch (args[i]){
+                    case "-r":
+                        String resoluçãoTemporal = args[i+1];
+                        break;
+                    case "-di":
+                        String dataInicial = args[i+1];
+                        break;
+                    case "-df":
+                        String dataFinal = args[i+1];
+                        break;
+                }
+            }
 
-        lerFicheiroCSV();
 
+            if (verificarExtensãoFicheiro(args[args.length - 2]).equals("csv")){
+                String ficheiroEntrada = args[args.length - 2];
+            } else System.out.println("Ficheiro de entrada inválido");
+
+            if (verificarExtensãoFicheiro(args[args.length - 1]).equals("txt")){
+                String ficheiroSaida = args[args.length - 1];
+
+            } else System.out.println("Ficheiro de saida inválido");
+
+        } else System.out.println("Quantidade de argumentos inválida");
+
+
+    }
+    public static String verificarExtensãoFicheiro (String nomeFicheiro){
+        String ext = "";
+
+        int i = nomeFicheiro.lastIndexOf('.');
+        if (i > 0) {
+            ext = nomeFicheiro.substring(i+1);
+        }
+
+        return ext;
     }
 
     public static void lerFicheiroCSV() throws FileNotFoundException {
