cd913e08411d4d201cce152f5af6fc7b85c09695
1211506 <1211506@isep.ipp.pt>
2022-01-09 16:53:19 +0000

    Leitura dos dados com datas

--- src/Projeto.java
+++ src/Projeto.java
@@ -5,45 +5,44 @@ public class Projeto {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        //LerFicheiroCSV();
-        int numeroDeLinhas = ContarLinhasDoFicheiro();
+        int numeroDeLinhas = ContarLinhasDoFicheiro() - 1;
 
         String[] listaDeDatas = new String[numeroDeLinhas];
+        listaDeDatas = PreencherListaDeDatas(listaDeDatas);
         int[] listaDeCasosNaoInfetados = new int[numeroDeLinhas];
-        listaDeCasosNaoInfetados = PreencherValoresDosDadosPrimeiraFase(1,listaDeCasosNaoInfetados);
+        listaDeCasosNaoInfetados = PreencherValoresDosDadosPrimeiraFase(1, listaDeCasosNaoInfetados);
         int[] listaDeInfetados = new int[numeroDeLinhas];
-        listaDeInfetados = PreencherValoresDosDadosPrimeiraFase(2,listaDeInfetados);
+        listaDeInfetados = PreencherValoresDosDadosPrimeiraFase(2, listaDeInfetados);
         int[] listaDeHospitalizados = new int[numeroDeLinhas];
-        listaDeHospitalizados = PreencherValoresDosDadosPrimeiraFase(3,listaDeHospitalizados);
+        listaDeHospitalizados = PreencherValoresDosDadosPrimeiraFase(3, listaDeHospitalizados);
         int[] listaDeInternadosUCI = new int[numeroDeLinhas];
-        listaDeInternadosUCI = PreencherValoresDosDadosPrimeiraFase(4,listaDeInternadosUCI);
+        listaDeInternadosUCI = PreencherValoresDosDadosPrimeiraFase(4, listaDeInternadosUCI);
         int[] listaDeMortes = new int[numeroDeLinhas];
-        listaDeMortes = PreencherValoresDosDadosPrimeiraFase(5,listaDeMortes);
+        listaDeMortes = PreencherValoresDosDadosPrimeiraFase(5, listaDeMortes);
 
         //por num modulo???
         if (args.length == 6 || args.length == 8) { // verificar se é um numero válido de argumentos
             for (int i = 0; i < args.length; i = i + 2) {
-                switch (args[i]){
+                switch (args[i]) {
                     case "-r":
-                        String resolucaoTemporal = args[i+1];
+                        String resolucaoTemporal = args[i + 1];
                         break;
                     case "-di":
-                        String dataInicial = args[i+1];
+                        String dataInicial = args[i + 1];
                         break;
                     case "-df":
-                        String dataFinal = args[i+1];
+                        String dataFinal = args[i + 1];
                         break;
                 }
             }
 
-            //LerFicheiroCSV();
             ContarLinhasDoFicheiro();
 
-            if (VerificarExtensãoFicheiro(args[args.length - 2]).equals("csv")){
+            if (VerificarExtensãoFicheiro(args[args.length - 2]).equals("csv")) {
                 String ficheiroEntrada = args[args.length - 2];
             } else System.out.println("Ficheiro de entrada inválido");
 
-            if (VerificarExtensãoFicheiro(args[args.length - 1]).equals("txt")){
+            if (VerificarExtensãoFicheiro(args[args.length - 1]).equals("txt")) {
                 String ficheiroSaida = args[args.length - 1];
 
             } else System.out.println("Ficheiro de saida inválido");
@@ -52,6 +51,7 @@ public class Projeto {
 
 
     }
+
     public static int[] PreencherValoresDosDadosPrimeiraFase(int tipoDeDado, int[] listaDeDados) throws FileNotFoundException {
 
         int count = 0;
@@ -61,23 +61,33 @@ public class Projeto {
         while (fin.hasNextLine()) {
             String line = fin.nextLine();
             String[] dados = line.split(splitBy);
+            listaDeDados[count] = Integer.parseInt(dados[tipoDeDado]);
             count++;
-            PreencherValorDosDadosSegundaFase(tipoDeDado, dados, listaDeDados,count);
-                }
+        }
+        fin.close();
 
         return listaDeDados;
     }
-    public static void PreencherValorDosDadosSegundaFase(int tiposDeDados, String[] dados, int[] listaDeDados, int posicao){
-        listaDeDados[posicao] = Integer.parseInt(dados[tiposDeDados]);
-        System.out.println(listaDeDados[posicao]);
+
+    public static String[] PreencherListaDeDatas(String[] listaDeDatas) throws FileNotFoundException {
+        int count = 0;
+        String splitBy = ",";
+        Scanner fin = new Scanner(new File("exemploRegistoNumerosCovid19.csv"));
+        fin.nextLine();
+        while (fin.hasNextLine()) {
+            String line = fin.nextLine();
+            String[] dados = line.split(splitBy);
+            listaDeDatas[count] = dados[0];
+            count++;
+
         }
 
+        fin.close();
+        return listaDeDatas;
+    }
 
-    public static String[] PreencherListaDeDatas(int tipoDeDados,String[] dados, String[] listaDeDatas) {
+    public static void PreencherListaDeDatasSegundaFase(String[] dados, String[] listaDeDatas){
 
-
-
-        return dados;
     }
 
     public static int ContarLinhasDoFicheiro() throws FileNotFoundException {
@@ -85,7 +95,7 @@ public class Projeto {
         Scanner fin = new Scanner(new File("exemploRegistoNumerosCovid19.csv"));
         int counter = 0;
 
-        while (fin.hasNextLine()){
+        while (fin.hasNextLine()) {
             counter++;
             fin.nextLine();
         }
@@ -93,34 +103,15 @@ public class Projeto {
         return counter;
     }
 
-    public static String VerificarExtensãoFicheiro(String nomeFicheiro){
+    public static String VerificarExtensãoFicheiro(String nomeFicheiro) {
         String ext = "";
 
         int i = nomeFicheiro.lastIndexOf('.');
         if (i > 0) {
-            ext = nomeFicheiro.substring(i+1);
+            ext = nomeFicheiro.substring(i + 1);
         }
 
         return ext;
     }
 
-   /* public static void LerFicheiroCSV() throws FileNotFoundException {
-
-        String line = "";
-        String splitBy = ",";
-
-        try {
-
-            BufferedReader br = new BufferedReader(new FileReader("exemploRegistoNumerosCovid19.csv"));
-            while ((line = br.readLine()) != null) {
-                String[] dados = line.split(splitBy);
-
-                System.out.println("data: " + dados[0] + " infetados: " + dados[2]);
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-//teste
-    }
-*/}
+}
