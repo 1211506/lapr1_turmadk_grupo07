b9310dd1e78272286b57d8ed66e2359631a9d5ef
PedroAMC <77537974+PedroAMC@users.noreply.github.com>
2022-01-22 16:28:00 +0000

    Add files via upload

---
+++ src/TestesUnitarios.java
@@ -0,0 +1,25 @@
+public class TestesUnitarios {
+    public static void main(String[] args) {
+
+
+    }
+
+    /*
+    private static boolean test_soma_dois_numeros(int op1,int op2, int expectedSoma){
+        if( Projeto.SomaDoisNumeros(op1, op2) == expectedSoma){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    /**
+     *  executa todos os testes
+
+    public static void executarTestes(){
+        System.out.println( "soma_dois_numeros:" + (test_soma_dois_numeros(1,2,3)?"OK":"NOT OK"+"\n") );
+        // etc...
+    }
+    */
+}
